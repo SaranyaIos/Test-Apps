37a5cc73bf3e341abaa8906286e177aadaef23c7
Saranya Vignesh <saranyachandran204@gmail.com>
2017-06-22 18:42:27 +0530

    Initial Commit

---
+++ TestApps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApps
+//
+//  Created by Saranya Vignesh on 22/06/17.
+//  Copyright © 2017 Saranya Vignesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApps
+//
+//  Created by Saranya Vignesh on 22/06/17.
+//  Copyright © 2017 Saranya Vignesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
